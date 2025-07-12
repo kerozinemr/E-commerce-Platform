b0b8be72fd1a50ce5526157604a45c6146642091
MazzX <155610819+kerozinemr@users.noreply.github.com>
2025-07-12 06:20:46 +0100

    fixing host

--- ecom/settings.py
+++ ecom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://ecom-cwu4.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = ['ecom-cwu4.onrender.com','127.0.0.1']
 
 
 # Application definition
