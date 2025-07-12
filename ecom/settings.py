7fed4643c60623cdd93032b5901dc7009a4fb8e2
MazzX <155610819+kerozinemr@users.noreply.github.com>
2025-07-12 06:18:00 +0100

    Configuring Render-postgres database

--- ecom/settings.py
+++ ecom/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='DATABASE_URL')
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
